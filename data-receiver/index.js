aee2ce678678b398c97442f8f606ffbd934f08ed
fdwraith <kevinzhang3702@gmail.com>
2019-10-25 23:28:22 -0400

    created a temporary saving service

---
+++ data-receiver/index.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+var PORT = 10000;
+
+var saveFilename = "somejsonthing.txt";
+var writeStream = fs.createWriteStream(saveFilename);
+
+app.post("/saveData", (req, res) => {
+    console.log(req.body);    
+    writeStream.write(JSON.stringify(req.body) + "\n");    
+    res.send("ok");
+});
+
+
+writeStream.once('open', () => {
+    app.listen(PORT);
+});
+
+
+
