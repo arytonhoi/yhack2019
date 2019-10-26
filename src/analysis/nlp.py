644c498ec26ae9586f247e7ce661822114e09526
Aryton Hoi <aryton.a.hoi@gmail.com>
2019-10-26 11:30:17 -0400

    added functionality to process list of input

--- src/analysis/nlp.py
+++ src/analysis/nlp.py
@@ -4,41 +4,77 @@ from google.cloud.language import enums
 from google.cloud.language import types
 
 class NLP:
-    # Constructor
-    # Instantiates a client
-    def __init__(self):
-        self.client = language.LanguageServiceClient()
-
-    # 
-    def get_whole_sentiment(self,content,language='en',
-                type_ = enums.Document.Type.PLAIN_TEXT, encoding_type=enums.EncodingType.UTF8):
- 
-        # call API to get entity analysis
-        document = {"content": content, "type": type_, "language": language}
-        api_response = self.client.analyze_sentiment(document, encoding_type=encoding_type)
-        return {'score':api_response.document_sentiment.score,
-                'magnitude':api_response.document_sentiment.magnitude}
+  # Constructor
+  # Instantiates a client
+  def __init__(self):
+    self.client = language.LanguageServiceClient()
 
 
-    def get_entities(self,content,salience_threshold,language='en',
-                type_=enums.Document.Type.PLAIN_TEXT, encoding_type=enums.EncodingType.UTF8):
-        
-        entities = []
+  # calls api to get the sentiment of the entire document
+  # returns dictionary of form:
+  # {
+  #   'score':double,
+  #   'magnitude':double
+  # }
+  def get_whole_sentiment(self,content,language='en',
+              type_ = enums.Document.Type.PLAIN_TEXT, encoding_type=enums.EncodingType.UTF8):
+    # call API to get entity analysis
+    document = {"content": content, "type": type_, "language": language}
+    api_response = self.client.analyze_sentiment(document, encoding_type=encoding_type)
+    return {'score':api_response.document_sentiment.score,
+            'magnitude':api_response.document_sentiment.magnitude}
 
-        # call API to get entity analysis
-        document = {"content": content, "type": type_, "language": language}
-        api_response = self.client.analyze_entity_sentiment(document, encoding_type=encoding_type)
 
-        for entity in api_response.entities:
+  # calls api to get individual entity information
+  # returns list of dictionaries, each of form:
+  # {
+  #   'value': string, 
+  #   'type': string, 
+  #   'salience': double, 
+  #   'sentiment': {
+  #     'score': double, 
+  #    'magnitude': double
+  #   }
+  # }
+  def get_entities(self,content,salience_threshold,language='en',
+              type_=enums.Document.Type.PLAIN_TEXT, encoding_type=enums.EncodingType.UTF8):
+    entities = []
+
+    # call API to get entity analysis
+    document = {"content": content, "type": type_, "language": language}
+    api_response = self.client.analyze_entity_sentiment(document, encoding_type=encoding_type)
+
+    for entity in api_response.entities:
+        entity_salience = entity.salience
+        if entity_salience > salience_threshold:
+            entities.append({'value': entity.name, 
+                            'type': enums.Entity.Type(entity.type).name, 
+                            'salience': entity.salience, 
+                            'sentiment': {'score': entity.sentiment.score, 
+                                          'magnitude': entity.sentiment.magnitude}
+                            })
+    return entities 
 
 
-            entity_salience = entity.salience
-            if entity_salience > salience_threshold:
-
-                entities.append({'value': entity.name, 
-                                'type': enums.Entity.Type(entity.type).name, 
-                                'salience': entity.salience, 
-                                'sentiment': {'score': entity.sentiment.score, 
-                                            'magnitude': entity.sentiment.magnitude}})
-
-        return entities
+  # calls api (via helper methods) to get both document level sentiment and
+  # individual entity level information
+  # returns dictionary of form:
+  # {
+  #   'document_sentiment': {
+  #     'score':double, 
+  #     'magnitude':double
+  #   },
+  #   'entities': {
+  #     'value': string, 
+  #     'type': string, 
+  #     'salience': double, 
+  #     'sentiment': {
+  #       'score': double, 
+  #       'magnitude': double}
+  #     }
+  #   }
+  # }
+  def analyze(self,content,salience_threshold,language='en',
+            type_=enums.Document.Type.PLAIN_TEXT, encoding_type=enums.EncodingType.UTF8):
+    return {'document_sentiment': self.get_whole_sentiment(content['content']),
+            'entities': self.get_entities(content['content'], salience_threshold)}
--- src/analysis/test.py
+++ src/analysis/test.py
@@ -6,20 +6,37 @@ from nlp import NLP
 import argparse
 import json
 
+
+class Media_Analyzer:
+  # constructor
+  def __init__(self):
+    self.nlp = NLP()
+
+
+  # analyze a list of media posts
+  def analyze_media_posts(self, input_json, output_json, salience_threshold=0.001):
+    with open(input_json) as f:
+      data = json.load(f)
+
+      nlp_results = []
+      for media_post in data['data']:
+        nlp_result = self.nlp.analyze(media_post, salience_threshold)
+        nlp_result['username'] = media_post['username']
+        nlp_results.append(nlp_result)
+
+      results = {'results':nlp_results}
+      with open(output_json, 'w') as fp:
+        json.dump(results, fp)
+
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("json_file")
+  parser.add_argument("--out", default='test_output.json')
   args = parser.parse_args()
 
-  nlp = NLP()
-  threshold = 0.01
+  input_json = args.json_file
+  output_json = args.out
 
-  json_file = args.json_file
-  with open(json_file) as f:
-    data = json.load(f)
-    for obj in data['data']:
-      
-      dict_json = {'document_sentiment': nlp.get_whole_sentiment(obj['content']),
-                    'entities': nlp.get_entities(obj['content'], threshold)}
-      with open('test_output.json', 'w') as fp:
-        json.dump(dict_json, fp)
+  media_analyzer = Media_Analyzer()  
+  media_analyzer.analyze_media_posts(input_json, output_json)
