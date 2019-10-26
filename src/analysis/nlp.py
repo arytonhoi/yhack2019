9c2443a63d0851c0af1dd4f7bd4c6f83171df22b
Paul Rhee <rhee.p@MacBook-Pro.local>
2019-10-26 05:37:21 -0400

    nlp works on single json input

--- src/analysis/nlp.py
+++ src/analysis/nlp.py
@@ -22,23 +22,12 @@ class NLP:
 
     def get_entities(self,content,salience_threshold,language='en',
                 type_=enums.Document.Type.PLAIN_TEXT, encoding_type=enums.EncodingType.UTF8):
-        # print('getting topics')
-        # topics list
+        
         entities = []
 
         # call API to get entity analysis
         document = {"content": content, "type": type_, "language": language}
         api_response = self.client.analyze_entity_sentiment(document, encoding_type=encoding_type)
-
-        # loop over entities in api response
-
-        # # Detects the sentiment of the text
-        # sentiment = client.analyze_sentiment(document=document).document_sentiment
-
-        # print('Text: {}'.format(text))
-        # print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-
-        
 
         for entity in api_response.entities:
 
