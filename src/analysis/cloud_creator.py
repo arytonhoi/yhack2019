6224019f1bb63bc59962ac7f224ef0ba20bbad8d
Paul Rhee <rhee.p@MacBook-Pro.local>
2019-10-26 17:49:51 -0400

    added documentation to cloud creator

--- src/analysis/cloud_creator.py
+++ src/analysis/cloud_creator.py
@@ -14,26 +14,36 @@ def blue_color_func(word, font_size, position, orientation, random_state=None, *
 def red_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
     return "hsl(0, 100%%, %d%%)" % random.randint(40, 80)
 
+# creates a cloud or rectangle of key words
 class WordCloudGenerator:
 
     def __init__(self):
         pass
 
-
+    # create a word cloud based on given text file
+    # the user can specify the name and format in which they want to store
+    # in the export_name field
+    # sentiment is 'positive' (blue) or 'negatve' (red)
+    # mask_file is optional - use to outline into cloud shape or leave alone for rectangle shape
     def generate_image(self, text_file, export_name, sentiment, mask_file=None):
         text = open(text_file).read()
+
+        # create based on mask file
         if (mask_file is not None):
             mask_image = np.array(Image.open(mask_file))
             wc = WordCloud(background_color="white", mask=mask_image)
         else:
             wc = WordCloud(background_color="white")
         wc.generate(text)
+
+        # color change
         if (sentiment is "positive"):
             wc.recolor(color_func=blue_color_func)
         else:
             wc.recolor(color_func=red_color_func)
+
+        # export
         wc.to_file(export_name)
-        return wc
 
 
 image = WordCloudGenerator()
