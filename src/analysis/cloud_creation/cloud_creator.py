2793f5d2a64b153bfe15b6ad723f2c177d759099
Paul Rhee <rhee.p@MacBook-Pro.local>
2019-10-27 01:59:48 -0400

    more picture stuff hehe xd

--- src/analysis/cloud_creation/cloud_creator.py
+++ src/analysis/cloud_creation/cloud_creator.py
@@ -14,7 +14,29 @@ import math
 import numpy as np
 
 # words to filter
-useless_words = ["flanes", "flane", "flight", "flights", "airline", "home", "jetblue", "nothing", "jet blue", "way", "jfk", "boston", "one"]
+useless_words = ["planes", "plane", "airplane", "flight", "flights", "airline", "home", "jetblue", 
+"nothing", "jet blue", "way", "jfk", "boston", "one", "anything", "airlines", "reason", "something"]
+
+# read json data and retrieve keywords, then store in a big list
+def getPositiveKeywords(json_file):
+    with open(json_file) as f:
+        data = json.load(f)
+        keyword_list = []
+        for item in data["results"]:
+            if item['sentiment_score'] >= 0.25:
+                keyword_list.extend(item['keywords'])
+        keyword_list = filter(lambda i: i.lower()  not in useless_words, keyword_list)
+        return keyword_list
+
+def getNegativeKeywords(json_file):
+    with open(json_file) as f:
+        data = json.load(f)
+        keyword_list = []
+        for item in data["results"]:
+            if item['sentiment_score'] <= -0.25:
+                keyword_list.extend(item['keywords'])
+        keyword_list = filter(lambda i: i.lower()  not in useless_words, keyword_list)
+        return keyword_list
 
 # create shades of blue for word cloud to represent positive attributes
 def blue_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
@@ -22,13 +44,23 @@ def blue_color_func(word, font_size, position, orientation, random_state=None, *
 
 # create shades of red for word cloud to represent negative attributes
 def red_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
-    return "hsl(0, 100%%, %d%%)" % random.randint(40, 80)
+    return "hsl(0, 100%%, %d%%)" % random.randint(20, 60)
 
 # creates a cloud or rectangle of key words
 class WordCloudGenerator:
 
     def __init__(self):
         pass
+
+    def create_keywords_file(self, json_file, destination, option):
+        if option == "positive":
+            list_keywords = getPositiveKeywords(json_file)
+        else:
+            list_keywords = getNegativeKeywords(json_file)
+        cloud_file = open(destination, "w")
+        for item in list_keywords:
+            cloud_file.write(item)
+        cloud_file.close()
 
     # create a word cloud based on given text file
     # the user can specify the name and format in which they want to store
@@ -38,12 +70,15 @@ class WordCloudGenerator:
     def generate_image(self, text_file, export_name, sentiment, mask_file=None):
         text = open(text_file).read()
 
+        for word in useless_words:
+            text.replace(word, "")
+
         # create based on mask file
         if (mask_file is not None):
             mask_image = np.array(Image.open(mask_file))
-            wc = WordCloud(background_color="white", mask=mask_image)
+            wc = WordCloud(background_color="white", mask=mask_image, relative_scaling=0.5)
         else:
-            wc = WordCloud(background_color="white")
+            wc = WordCloud(background_color="white", relative_scaling=1)
         wc.generate(text)
 
         # color change
@@ -57,26 +92,8 @@ class WordCloudGenerator:
 
 # create instance of WordCloud
 image = WordCloudGenerator()
-image.generate_image("cons.txt", "ex.png", "positive", "fb.png")
-
-# read json data and retrieve keywords, then store in a big list
-def getPositiveKeywords(json_file):
-    with open(json_file) as f:
-        data = json.load(f)
-        keyword_list = []
-        for item in data["results"]:
-            if item['sentiment_score'] >= 0.25:
-                keyword_list.extend(item['keywords'])
-        return keyword_list
-
-def getNegativeKeywords(json_file):
-    with open(json_file) as f:
-        data = json.load(f)
-        keyword_list = []
-        for item in data["results"]:
-            if item['sentiment_score'] <= -0.25:
-                keyword_list.extend(item['keywords'])
-        return keyword_list
+image.create_keywords_file('refined_big_output.json', 'cloud.txt', "negative")
+image.generate_image("cloud.txt", "ex.png", "negative", 'cloud.png')
 
 # read data from a json file and create a good lookin graph of the top five
 # -1 - -0.25 is negative, 0.25 - 1.0 is positive
@@ -101,7 +118,7 @@ def findTop(keywords, num):
         return top_list
 
 # graph result of top results
-def createTopGraph(top_list):
+def createTopGraph(top_list, sentiment):
 
     # x values
     list_words = []
@@ -129,7 +146,10 @@ def createTopGraph(top_list):
     # plt.title("All-Time Top 5 Words", fontsize = 20, fontweight='bold', color="#001e5e", loc='left')
     # plt.text(0.01, 0.9, s="All-Time Top 5 Words", fontsize = 20, fontweight='bold', color="#001e5e", transform=plt.gcf().transFigure)
     # create bar graph
-    plt.barh(y_pos, list_count, height=0.2, align='center', color="#00b3ff")
+    if sentiment == "positive":
+        plt.barh(y_pos, list_count, height=0.2, align='center', color="#00b3ff")
+    else:
+        plt.barh(y_pos, list_count, height=0.2, align='center', color="#eb3a34")
     # set labels on left for words
     plt.yticks(y_pos, list_words, fontweight='bold', color="#001e5e")
     # leave labels on x axis blank for clarity
@@ -148,10 +168,8 @@ def createTopGraph(top_list):
     
     # save
     plt.savefig('graph.png')
-    plt.show()
 
 # run
 list_pos = getPositiveKeywords('refined_big_output.json')
-list_20 = findTop(list_pos, 30)
-createTopGraph(list_20)
-    
+list_7 = findTop(list_pos, 7)
+createTopGraph(list_7, "positive")
