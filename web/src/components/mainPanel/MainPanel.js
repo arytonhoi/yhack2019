e6ad4e99092503fc7dd376ca4f2af57cc7f6ccf7
fdwraith <kevinzhang3702@gmail.com>
2019-10-26 23:37:45 -0400

    added toggle panel

--- web/src/components/mainPanel/MainPanel.js
+++ web/src/components/mainPanel/MainPanel.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./MainPanel.css";
 
+import ToggleablePanel from "./ToggleablePanel";
 
 function MainPanel(props) {
-  return <div style={{ "flex": 3 }}>Main Panel here</div>;
+  const options = {
+    on: {
+      name: "Positive Reviews",
+      render: () => <div>Something goes here</div>
+    },
+    off: {
+      name: "Negative Reviews",
+      render: () => <div>Something else goes here</div>
+    }
+  };
+  return <ToggleablePanel on={options.on} off={options.off} />;
 }
 
 export default MainPanel;
